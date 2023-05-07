0c0f2f7539aa9d03b5a59265770b97fc1395a06d
flindip <18757495+flindip@users.noreply.github.com>
2023-05-06 23:31:10 -0400

    added pen trail through mouseover event and button

--- myscripts.js
+++ myscripts.js
@@ -1,20 +1,25 @@
+         
+
 const display=document.getElementById('display');
-   function createGrid(){
-   for(let i=0; i<16; i++){
+   function createGrid(x){
+   for(let i=0; i<x; i++){
        let columns=document.createElement('div');
        columns.classList.add('columns');
        display.appendChild(columns);
-    if(i===15){
+    if(i===(x-1)){
         display.lastChild.style="border:none"};
     for(let i=0; i<16; i++){
        let rows=document.createElement('div');
        rows.classList.add('rows');
        columns.appendChild(rows);
-    if(i===15){
+    if(i===(x-1)){
     columns.lastChild.style="border:none";
         }}}};
-     createGrid();
+     createGrid(16);
     //creates 16x16 grid for display div 
-     const columns=document.getElementsByClassName('column');
-     const rows=document.getElementsByClassName('rows');
- 
+     const columns=document.querySelectorAll('.columns');
+     const rows=document.querySelectorAll('.rows');
+   //you're calling array like objects-you need to loop to make them change
+   for(const row of rows){
+   row.addEventListener('mouseenter', (e)=>e.target.style="background-color:black")
+};
